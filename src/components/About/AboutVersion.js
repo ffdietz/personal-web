9eaf1d572772f533d018153a9ebeb1ceeefd0afb
fdietz <felipefierrod@gmail.com>
2021-02-17 23:52:00 +0100

    about txt

---
+++ src/components/About/AboutVersion.js
@@ -0,0 +1,15 @@
+
+export const AboutVersions = [
+
+    {
+        language: 'esp',
+        text: 'Es artista visual e investigador chileno, actualmente con base en Berlin. Su temprana incursión en ciencias y posterior formación como artista fotógrafo, lo ha llevado a trabajar las relaciones entre las artes visuales y el lenguaje científico, utilizando la tecnologías eléctricas/electrónicas como puente comunicante.',
+        cName: 'biography-txt'
+    },
+    {
+        language: 'eng',
+        text: 'He is a Chilean visual artist and researcher, currently based in Berlin. His early foray into science and subsequent training as a photographer artist, has led him to work on the relationships between visual arts and scientific language, using electrical / electronic technologies as a communicating bridge.',
+        cName: 'biography-txt'
+    }
+
+]
